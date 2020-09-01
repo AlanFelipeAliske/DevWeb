7cdbfed01ba202c15817e9587d57e367917044c8
Alan Felipe <alanfelipealiske@gmail.com>
2020-09-01 09:06:21 -0300

    Adicionando tema dark

--- pages/scriptContato.js
+++ pages/scriptContato.js
@@ -17,9 +17,30 @@ function id( el ){
 return document.getElementById( el );
 }
 window.onload = function(){
-id('telefone').onkeyup = function(){
-  mascara( this, mtel );
+  id('telefone').onkeyup = function(){
+    mascara( this, mtel );
+  }
 }
-}
+
+
+
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
 
 
